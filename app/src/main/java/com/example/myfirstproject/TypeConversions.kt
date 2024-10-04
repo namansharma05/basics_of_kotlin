fa06589e66d8a1438df533024e599bead4108985
Naman Sharma <namanks0511@gmail.com>
2024-10-04 20:17:37 +0530

    added TypeConversition file

---
+++ app/src/main/java/com/example/myfirstproject/TypeConversions.kt
@@ -0,0 +1,28 @@
+package com.example.myfirstproject
+/*
+
+    toByte()
+    toShort()
+    toInt()
+    toLong()
+    toFloat()
+    toDouble()
+
+ */
+fun main(args: Array<String>) {
+//    var a : Byte = 127;
+//    var b : Int = a.toInt();
+//    var c : Double = b.toDouble();
+//
+//    println(a);
+//    println(b);
+//    println(c);
+
+    var a : Double = 300.23;
+    var b : Int = a.toInt();
+    var c : Byte = b.toByte();
+
+        println(a);
+    println(b);
+    println(c);
+}
