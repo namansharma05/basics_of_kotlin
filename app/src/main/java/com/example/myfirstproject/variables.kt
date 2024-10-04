6936ff9fc26de55da2eae8e50d422473d29b771b
Naman Sharma <namanks0511@gmail.com>
2024-10-04 20:03:11 +0530

    added dataTypes and variables file

---
+++ app/src/main/java/com/example/myfirstproject/variables.kt
@@ -0,0 +1,16 @@
+package com.example.myfirstproject
+
+fun main(args: Array<String>) {
+    var a = 10
+    var b = 20
+    var c = a+b
+    println(a+b)
+
+//    var age = 20;
+//    age = 30;
+//    println(age)
+//
+    val year = 2019;
+//    year = 2020;
+
+}
