d7e80e343b93b2747f012e7fde777c8c6483f6f3
Naman Sharma <namanks0511@gmail.com>
2024-10-05 17:23:29 +0530

    added StringDataType file

---
+++ app/src/main/java/com/example/myfirstproject/StringDataType.kt
@@ -0,0 +1,17 @@
+package com.example.myfirstproject
+
+fun main(args: Array<String>) {
+    var name : String = "Naman";
+    var surname : String = " Sharma";
+    println(name)
+    println(surname)
+    println(name.length)
+    println(name.equals(surname))
+    println(name.isEmpty())
+    println(name.isNotEmpty())
+    println(name.plus(surname))
+    println(name.lowercase())
+    println(name.uppercase())
+    println(surname.trim())
+
+}
